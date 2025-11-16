9cd46df4b710cb46cfe855b97cd2dd84e8cee277
Vitor Araujo <vitorgamer58@outlook.com>
2025-11-16 18:36:28 -0300

    Adjust to amount be zero

--- src/domain/entities/Amount.ts
+++ src/domain/entities/Amount.ts
@@ -2,12 +2,10 @@ import { z } from "zod"
 
 const Amount = z.object({
   currency: z.string({ description: "Currency in format ISO 4217, example: (PYG, BRL, USD)" }),
-  value: z
-    .number({
-      description:
-        "The amount value, (negative if is a expense, if it is a commercial/service invoice, consider it an expense.)"
-    })
-    .refine((val) => val !== 0, { message: "Amount value cannot be zero" })
+  value: z.number({
+    description:
+      "The amount value, (negative if is a expense, if it is a commercial/service invoice, consider it an expense.)"
+  })
 })
 
 export { Amount }
